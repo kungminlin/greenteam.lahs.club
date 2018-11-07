4f7f185f777bb8a47f5294beb7cf5b46f9aa9ebc
kungminlin <mml0816@gmail.com>
2018-11-07 03:45:01 +0000

    remove extra directory

--- _all/tedx/assets/templates/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-	<p>&copy; 2018 <a href="https://generationon.lahs.club">LAHS generationOn Club</a> | Website created by Kung-Min Lin in association with <a href="https://lahs.club">LAHS Hack Club</a></p>
-</footer>
--- _all/tedx/old_versions/assets/templates/header.php
+++
@@ -1,7 +0,0 @@
-<div id="header-banner">
-	<div id="header-title">
-		<div class="main"><img src="assets/images/tedx-logo.png" /><!-- TED<sup>x</sup> --></div>
-		<div class="subtitle"><span>LAHS</span></div>
-	</div>
-	<div id="proceed-page"><i class="fa fa-angle-down"></i></div>
-</div>
