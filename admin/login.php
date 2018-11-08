fa835890063731926d1e192939a9f9865016dbb6
kungminlin <mml0816@gmail.com>
2018-11-07 21:28:16 -0800

    optimize login

--- admin/login.php
+++ admin/login.php
@@ -13,24 +13,35 @@ if ($link === false) {
   die("Connect Failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM admins";
+$username = mysqli_real_escape_string($link, $_POST['username']);
+
+$sql = "SELECT * FROM admins WHERE username = $username";
 $result = mysqli_query($link, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-        if ($_POST['username'] == $row['username'] && md5($_POST['password']) == $row['password']) {
-          $_SESSION['logged_in'] = array(
-            'username' => $row['username'],
-            'first_name' => $row['first_name'],
-            'last_name' => $row['last_name']
-          );
-          $response->error = "";
-          break;
-        }
-        else {
-          $response->error = "Incorrect username or password.";
-        }
-    }
+  $user = mysqli_fetch_assoc($result);
+  $_SESSION['logged_in'] = array(
+    'username' => $user['username'],
+    'first_name' => $user['first_name'],
+    'last_name' => $user['last_name']
+  );
+  $response->error = "";
+    // while($row = mysqli_fetch_assoc($result)) {
+    //     if ($_POST['username'] == $row['username'] && md5($_POST['password']) == $row['password']) {
+    //       $_SESSION['logged_in'] = array(
+    //         'username' => $row['username'],
+    //         'first_name' => $row['first_name'],
+    //         'last_name' => $row['last_name']
+    //       );
+    //       $response->error = "";
+    //       break;
+    //     }
+    //     else {
+    //       $response->error = "Incorrect username or password.";
+    //     }
+    // }
+} else {
+  $response->error = "Incorrect username or password.";
 }
 
 echo $response->error;
